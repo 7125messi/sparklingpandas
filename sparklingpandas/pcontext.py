f7e76b5a462ead88894b13372cbd384815360e72
Juliet Hougland <not@myemail.com>
2015-07-16 17:17:49 -0500

    Add pylint and fix errors.

--- sparklingpandas/pcontext.py
+++ sparklingpandas/pcontext.py
@@ -151,7 +151,7 @@ class PSparkContext():
 
     def table(self, table):
         """Returns the provided table as a L{Dataframe}"""
-        return Dataframe.from_spark_df(self.sql_ctx.table(query))
+        return Dataframe.from_spark_df(self.sql_ctx.table(table))
 
     def from_schema_rdd(self, schemaRDD):
         return Dataframe.from_spark_df(schemaRDD)
@@ -190,7 +190,7 @@ class PSparkContext():
             for filename, contents in files:
                 yield pandas.read_json(sio(contents), *args, **kwargs)
 
-        return dataframe.fromRDD(
+        return Dataframe.from_spark_df(
             self.sc.wholeTextFiles(name).mapPartitionsWithIndex(json_file))
 
     def stop(self):
